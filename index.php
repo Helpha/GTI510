6c709c69b6c3504cbef0bfc93afe6331395847a0
fufu0909 <aleyo36@msn.com>
2016-02-12 12:14:38 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Page d'accueil</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/script.js"></script>
+</head>
+<body>
+<?php 
+	echo "Hello world";
+?>
+
+</body>
+</html>
