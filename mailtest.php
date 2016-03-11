5cbac8bbb6f1a85d29c7d1a90c0b0ffaf51859c7
TechFred <comptes@techfred.ca>
2016-03-11 14:41:13 -0500

    Fonctionnel.

--- mailtest.php
+++ mailtest.php
@@ -1,41 +1,39 @@
 <?php
    
    class MailSMTP{
+      private  $USERNAME = "autobookets@gmail.com";
+      private  $PASSWORD = "4m7Wt9nL2vCy";
+      private  $HOST = "smtp.gmail.com";
+      private  $PORT = 587;
+      private  $SENDER = "BookÉTS";
+      private  $FROMEMAIL = "autobookets@gmail.com";
+      private $mail;
       
-      
-      function __construct($email, $messageBody) {
-         
-         $USERNAME = "autobookets@gmail.com";
-         $PASSWORD = "4m7Wt9nL2vCy";
-         $HOST = "smtp.gmail.com";
-         $PORT = 587;
-         $SUBJECT = "BookÉTS";
-         
-         
+      function __construct() {
          require_once("PHPMailer/PHPMailerAutoload.php");
          
          $mail = new PHPMailer();
          $mail->IsSMTP();
          $mail->CharSet = 'UTF-8';
-         
-         $mail->Host       = $HOST; // SMTP server example
-         $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-         $mail->SMTPAuth   = true;                  // enable SMTP authentication
-         $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
-         $mail->Port       = $PORT;                    // set the SMTP port for the GMAIL server
-         $mail->Username   = $USERNAME; // SMTP account username example
-         $mail->Password   = $PASSWORD;        // SMTP account password example
-         $mail->MsgHTML($messageBody);
-         $mail->AddAddress($email);
-         $mail->Subject    = $SUBJECT;
-         $mail->Send();
+         $mail->Host       = $this->HOST; 
+         $mail->SMTPDebug  = 0;                     
+         $mail->SMTPAuth   = true; 
+         $mail->SMTPSecure = "tls";  
+         $mail->Port       = $this->PORT;  
+         $mail->Username   = $this->USERNAME; 
+         $mail->Password   = $this->PASSWORD;    
+         $mail->SetFrom($this->FROMEMAIL, $this->SENDER);
+         $this->mail = $mail;
       }
-      public function send(){
+      public function send($email, $messageBody, $subject){
+         $this->mail->Subject = $subject;
+         $this->mail->MsgHTML($messageBody);
+         $this->mail->AddAddress($email);
+         return $this->mail->Send();
          
       }
    }
-   echo ($Email = new MailSMTP("frederic@somci.ca","allo")) ? 'true' : 'false';
-   $Email->send();
+   $Email = new MailSMTP();
+   echo ($Email->send("marqfrederic@gmail.com","allo","Reservation")) ? 'true' : 'false';
    
 ?>
-Done
