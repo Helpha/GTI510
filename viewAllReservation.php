7aa4a80c119eb4fb48b75ab935b28df281f02118
TechFred <comptes@techfred.ca>
2016-03-11 16:56:39 -0500

    reservationRest.php. C'est modifié partout.

--- viewAllReservation.php
+++ viewAllReservation.php
@@ -218,7 +218,7 @@
 	
 	$('.deleteConfirmation').click(function(){
 		$.ajax({
-        url:'reservationREST.php',
+        url:'reservationRest.php',
         type:'POST',
         data:{id : $(this).attr('data-value'), method : "DELETE"},
         success:function(data){
